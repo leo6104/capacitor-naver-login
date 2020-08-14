3bc0ccdb074a4405d4211e65c2a08456e219b5a5
Heo Sangmin <heo@mapiacompany.com>
2020-08-14 13:50:54 +0900

    feat(dist): add dist folder in git

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,21 @@
+import { WebPlugin } from '@capacitor/core';
+import { NaverPlugin } from './definitions';
+export declare class NaverWeb extends WebPlugin implements NaverPlugin {
+    constructor();
+    getState(): Promise<{
+        state: string;
+    }>;
+    login(): Promise<{
+        accessToken: string;
+        refreshToken: string;
+        expiresAt: string;
+        tokenType: string;
+    }>;
+    logout(): Promise<void>;
+    logoutAndDeleteToken(): Promise<void>;
+    refreshAccessToken(): Promise<{
+        accessToken: string;
+    }>;
+}
+declare const Naver: NaverWeb;
+export { Naver };
---
+++ dist/esm/web.js
@@ -0,0 +1,52 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { registerWebPlugin, WebPlugin } from '@capacitor/core';
+export class NaverWeb extends WebPlugin {
+    constructor() {
+        super({
+            name: 'Naver',
+            platforms: ['web'],
+        });
+    }
+    getState() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return { state: 'Not Implemeneted Yet' };
+        });
+    }
+    login() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return {
+                accessToken: 'Not Implemented Yet',
+                refreshToken: 'Not Implemented Yet',
+                expiresAt: 'Not Implemented Yet',
+                tokenType: 'Barear'
+            };
+        });
+    }
+    logout() {
+        return __awaiter(this, void 0, void 0, function* () {
+            throw new Error('Not Implemented Yet');
+        });
+    }
+    logoutAndDeleteToken() {
+        return __awaiter(this, void 0, void 0, function* () {
+            throw new Error('Not Implemented Yet');
+        });
+    }
+    refreshAccessToken() {
+        return __awaiter(this, void 0, void 0, function* () {
+            return { accessToken: 'Not Implemented Yet' };
+        });
+    }
+}
+const Naver = new NaverWeb();
+export { Naver };
+registerWebPlugin(Naver);
+//# sourceMappingURL=web.js.map
