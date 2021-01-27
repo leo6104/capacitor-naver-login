02cd86762144e69ba9ea906fd41ba8bd8b9fc3ac
Heo Sangmin <heo@mapiacompany.com>
2021-01-27 22:49:45 +0900

    feat(ios): add login method in swift

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -8,51 +8,80 @@ import NaverThirdPartyLogin
  */
 @objc(Naver)
 public class Naver: CAPPlugin {
-    private let naverLogin = NaverThirdPartyLoginConnection.getSharedInstance()
+    private let instance = NaverThirdPartyLoginConnection.getSharedInstance()
+    var signInCall: CAPPluginCall?
 
     public override func load() {
         // 네이버 앱으로 인증하는 방식을 활성화
         if let isNaverAppOauthEnable = getConfigValue("isNaverAppOauthEnable") as? Bool {
-            naverLogin?.isNaverAppOauthEnable = isNaverAppOauthEnable
+            instance?.isNaverAppOauthEnable = isNaverAppOauthEnable
         }
 
         // SafariViewController에서 인증하는 방식을 활성화
         if let isInAppOauthEnable = getConfigValue("isInAppOauthEnable") as? Bool {
-            naverLogin?.isInAppOauthEnable = isInAppOauthEnable
+            instance?.isInAppOauthEnable = isInAppOauthEnable
         }
 
         // 인증 화면을 iPhone의 세로 모드에서만 사용하기
         if (getConfigValue("isOnlyPortraitSupportedInIphone") as? Bool) != nil {
-            naverLogin?.isOnlyPortraitSupportedInIphone()
+            instance?.isOnlyPortraitSupportedInIphone()
         }
 
         // 네이버 아이디로 로그인하기 설정
         // 애플리케이션을 등록할 때 입력한 URL Scheme
         if let serviceUrlScheme = getConfigValue("SERVICE_URL_SCHEME") as? String {
-            naverLogin?.serviceUrlScheme = serviceUrlScheme;
+            instance?.serviceUrlScheme = serviceUrlScheme;
         }
         // 애플리케이션 등록 후 발급받은 클라이언트 아이디
         if let consumerKey = getConfigValue("OAUTH_CLIENT_ID") as? String {
-            naverLogin?.consumerKey = consumerKey;
+            instance?.consumerKey = consumerKey;
         }
         // 애플리케이션 등록 후 발급받은 클라이언트 시크릿
         if let consumerSecret = getConfigValue("OAUTH_CLIENT_SECRET") as? String {
-            naverLogin?.consumerSecret = consumerSecret;
+            instance?.consumerSecret = consumerSecret;
         }
 
         // 애플리케이션 이름
         if let oAuthClientName = getConfigValue("OAUTH_CLIENT_NAME") as? String {
-            naverLogin?.appName = oAuthClientName;
+            instance?.appName = oAuthClientName;
         }
+
+        instance?.delegate = self;
     }
 
     @objc func login(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-
+        signInCall = call;
         DispatchQueue.main.async {
-            call.resolve([
-                "value": value
-            ])
+            self.instance?.requestThirdPartyLogin();
         }
+    }
+}
+
+extension Naver: NaverThirdPartyLoginConnectionDelegate {
+    // 로그인에 성공한 경우 호출
+    public func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        var userData: [String: Any] = [
+            "accessToken": instance?.accessToken,
+            "refreshToken": instance?.refreshToken,
+            "expiresAt": instance?.accessTokenExpireDate,
+            "tokenType": instance?.tokenType
+        ];
+        signInCall?.resolve(userData);
+    }
+
+    // referesh token
+    public func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+        <#code#>
+    }
+
+    // 로그아웃
+    public func oauth20ConnectionDidFinishDeleteToken() {
+        <#code#>
+    }
+
+    // 모든 error
+    public func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
+        <#code#>
+        signInCall?.reject(error.localizedDescription);
     }
 }
