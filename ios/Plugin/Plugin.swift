32d427be28ac7f54416d9219105a4c92fd36e211
Heo Sangmin <heo@mapiacompany.com>
2021-01-27 21:14:50 +0900

    feat: consider optional variable

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -13,37 +13,36 @@ public class Naver: CAPPlugin {
     public override func load() {
         // 네이버 앱으로 인증하는 방식을 활성화
         if let isNaverAppOauthEnable = getConfigValue("isNaverAppOauthEnable") as? Bool {
-            naverLogin.isNaverAppOauthEnable = isNaverAppOauthEnable
+            naverLogin?.isNaverAppOauthEnable = isNaverAppOauthEnable
         }
 
         // SafariViewController에서 인증하는 방식을 활성화
         if let isInAppOauthEnable = getConfigValue("isInAppOauthEnable") as? Bool {
-            naverLogin.isInAppOauthEnable = isInAppOauthEnable
+            naverLogin?.isInAppOauthEnable = isInAppOauthEnable
         }
 
         // 인증 화면을 iPhone의 세로 모드에서만 사용하기
-        if let isOnlyPortraitSupportedInIphone = getConfigValue("isOnlyPortraitSupportedInIphone") as? Bool {
-            naverLogin.isOnlyPortraitSupportedInIphone()
+        if (getConfigValue("isOnlyPortraitSupportedInIphone") as? Bool) != nil {
+            naverLogin?.isOnlyPortraitSupportedInIphone()
         }
 
         // 네이버 아이디로 로그인하기 설정
         // 애플리케이션을 등록할 때 입력한 URL Scheme
         if let serviceUrlScheme = getConfigValue("SERVICE_URL_SCHEME") as? String {
-            naverLogin.serviceUrlScheme = serviceUrlScheme;
+            naverLogin?.serviceUrlScheme = serviceUrlScheme;
         }
         // 애플리케이션 등록 후 발급받은 클라이언트 아이디
         if let consumerKey = getConfigValue("OAUTH_CLIENT_ID") as? String {
-            naverLogin.consumerKey = consumerKey;
+            naverLogin?.consumerKey = consumerKey;
         }
-        naverLogin.consumerKey = kConsumerKey
         // 애플리케이션 등록 후 발급받은 클라이언트 시크릿
         if let consumerSecret = getConfigValue("OAUTH_CLIENT_SECRET") as? String {
-            naverLogin.consumerSecret = consumerSecret;
+            naverLogin?.consumerSecret = consumerSecret;
         }
 
         // 애플리케이션 이름
         if let oAuthClientName = getConfigValue("OAUTH_CLIENT_NAME") as? String {
-            naverLogin.appName = oAuthClientName;
+            naverLogin?.appName = oAuthClientName;
         }
     }
 
